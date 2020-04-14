725fbc0b6a7d8264db6d1900e2665ae8d59d6738
LFalcone <34078649+LFalcone@users.noreply.github.com>
2020-04-14 16:07:35 -0700

    Added Some Constraints
    Shipping Address is now disabled when Pickup is selected and Phone and Card entries only allow numbers

--- checkout.js
+++ checkout.js
@@ -1,3 +1,23 @@
+var product;
+var method;
+
+function isDigit(event) {
+    var key = event.keyCode;
+    if(key > 47 && key < 58) {
+        return true;
+    }
+    return false;
+}
+
+function enableAddress() {
+    method = document.getElementById("method").value;
+    var address = document.getElementById("address");
+    if(method === "pickup") {
+        address.disabled = true;
+    } else {
+        address.disabled = false;
+    }
+}
 
 function submitInfo() {
     var infoForm = document.getElementById("infoForm");
@@ -6,7 +26,7 @@ function submitInfo() {
     var phone = infoForm.phone.value;
     var email = infoForm.email.value;
     var address = infoForm.address.value;
-    var method = infoForm.method.value;
+    method = infoForm.method.value;
     var card = infoForm.card.value;
     console.log("Hi, " + firstname + " " + lastname + "!");
 }

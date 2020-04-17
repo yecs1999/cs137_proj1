ad1b7339f49483650184e660eabecf4716b2ad73
LFalcone <34078649+LFalcone@users.noreply.github.com>
2020-04-17 16:25:26 -0700

    Now brings up the mail client and autofills an email

--- checkout.js
+++ checkout.js
@@ -19,14 +19,28 @@ function enableAddress() {
     }
 }
 
-function submitInfo() {
-    var infoForm = document.getElementById("infoForm");
-    var firstname = infoForm.firstname.value;
-    var lastname = infoForm.lastname.value;
-    var phone = infoForm.phone.value;
-    var email = infoForm.email.value;
-    var address = infoForm.address.value;
-    method = infoForm.method.value;
-    var card = infoForm.card.value;
+function submitCheckout() {
+    product = document.getElementById("product").innerText;
+    var checkoutForm = document.getElementById("checkoutForm");
+    var firstname = checkoutForm.firstname.value;
+    var lastname = checkoutForm.lastname.value;
+    var phone = checkoutForm.phone.value;
+    var email = checkoutForm.email.value;
+    var address = checkoutForm.address.value;
+    method = checkoutForm.method.value;
+    var card = checkoutForm.card.value;
     console.log("Hi, " + firstname + " " + lastname + "!");
+    var composeEmail = "mailto:" + email
+        + "?subject=Vending Cars Order Confirmation"
+        + "&body=Hi, " + firstname + " " + lastname + "! Your order for " + product + " has been placed ";
+    if(method === "pickup") {
+        composeEmail = composeEmail + "and will be ready for pickup. ";
+    } else if(method === "standard") {
+        composeEmail = composeEmail + "and will be delivered to " + address + " in 7 days. ";
+    } else {
+        composeEmail = composeEmail + "and will be delivered to " + address + " in 4 days. ";
+    }
+    composeEmail = composeEmail + "%0A%0AIf there is any delay, we will call your number at " + phone + ". "
+        + "%0A%0AThank you for shopping from Vending Cars!%0A%0A";
+    window.location.href = composeEmail;
 }

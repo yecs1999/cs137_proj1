fede2cc531006099f62c0827e0676b857a854219
Emerson Chow <emersoc@uci.edu>
2020-04-18 12:56:04 -0700

    Added alerts and required all fields to not be blank

--- checkout.js
+++ checkout.js
@@ -1,11 +1,13 @@
 var product;
 var method;
 
-function isDigit(event) {
+function isDigit(event, fieldName) {
     var key = event.keyCode;
+    
     if(key > 47 && key < 58) {
         return true;
     }
+    alert("Only digits are allowed in the " + fieldName + " field.");
     return false;
 }
 
@@ -19,6 +21,20 @@ function enableAddress() {
     }
 }
 
+//returns true and creates an alert message if there is an empty field, false otherwise
+function fieldsEmpty(listOfFields, listOfFieldsNames){
+    for (i = 0; i<listOfFields.length; ++i)
+    {
+        let clearedElement = listOfFields[i].trim();
+        if(clearedElement == "")
+        {
+            alert("Your " + listOfFieldsNames[i] + " cannot be blank. Please enter a " + listOfFieldsNames[i] + ".");
+            return true;
+        }
+    }
+    return false;
+}
+
 function submitCheckout() {
     product = document.getElementById("product").innerText;
     var checkoutForm = document.getElementById("checkoutForm");
@@ -29,6 +45,11 @@ function submitCheckout() {
     var address = checkoutForm.address.value;
     method = checkoutForm.method.value;
     var card = checkoutForm.card.value;
+
+    let allFields = [firstname, lastname, phone, email, address, card];
+    let allFieldsName = ["First Name", "Last Name", "Phone Number", "Email-Address", "Shipping Address", "Payment Info"];
+    if (!fieldsEmpty(allFields,allFieldsName)){
+        
     console.log("Hi, " + firstname + " " + lastname + "!");
     var composeEmail = "mailto:" + email
         + "?subject=Vending Cars Order Confirmation"
@@ -41,6 +62,7 @@ function submitCheckout() {
         composeEmail = composeEmail + "and will be delivered to " + address + " in 4 days. ";
     }
     composeEmail = composeEmail + "%0A%0AIf there is any delay, we will call your number at " + phone + ". "
-        + "%0A%0AThank you for shopping from Vending Cars!%0A%0A";
+        + "%0A%0AThank you for shopping with Vending Cars!%0A%0A";
     window.location.href = composeEmail;
+    }
 }
